d06e958ba0885daebdeb8dc8b66eafa9f4a37cf0
ra1lmuz <2003muzafarov@mail.ru>
2023-01-13 14:30:01 +0300

    Init react app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello</h1>
+      <p>Paragraph</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+/* function Header() {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+}
+
+export default Header; */
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+};
+
+export default Header;
